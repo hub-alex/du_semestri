1b8df1b57ed47316be8f324d6c7f593b36c6b573
hub-alex <75374916+hub-alex@users.noreply.github.com>
2024-02-09 14:23:14 +0000

    final

--- 01_linear.py
+++ 01_linear.py
@@ -1,19 +1,26 @@
-def make_massive():
+def make_massive()->list:
     mas = []
     mas_izm = int(input("Masīva izmērs: "))
     for i in range(mas_izm):
-        mas.append(input(f"Ievadiet massiva [{i}] elementu: "))
+        cipars = int(input(f"Ievadiet massiva [{i}] elementu: "))
+        mas.append(cipars)
     return mas
 
 
-def find_in_massive(mas:list): # asdasd
+def find_in_massive(mas:list):
+    """Meklejam ciparu masivā"""     
+    
     search = int(input("Meklejamais cipars: "))
+    
     for index, i in enumerate(mas):
-        print(i, index)
+        # print(i, search)
         if i == search:
             print(f"Atradam ciparu {search}, pēc indeksa {index}")
-        else:
-            print(f"Neatradam masivā ciparu {search}")
+            return
+    
+    print(f"Neatradam masivā ciparu {search}")
 
 
-find_in_massive(mas = make_massive()) 
+if __name__ == "__main__":
+    massive = make_massive()
+    find_in_massive(massive)
