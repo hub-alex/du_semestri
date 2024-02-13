299846a1f4fd8713b3dc3006956f750191bcba32
hub-alex <75374916+hub-alex@users.noreply.github.com>
2024-02-13 07:26:11 +0000

    with tests

--- tests.cpp
+++ tests.cpp
@@ -3,26 +3,50 @@
 #include <fstream>
 #include <ctime>
 #define N 100
+#define A 20
 
 using namespace std;
 
-int main(){
-int mas[N];
-int x, i;
+void save_file(int x){
+int i;    
 srand(time(NULL));
-
 ofstream(f1);
-cout<<"Ievadiet skaitļus: "<<endl;cin>>x;
-
 f1.open("name_of_file.txt", ios::out);
     if (f1){
     for (i=1;i<=x;i++){
         cout<<i<<endl;
         f1<<rand()%10+1<<" ";
 }
+cout<<"Fails ir izveidots!"<<endl;
 f1.close();
 }
 else cout<<"Kļūda!";
+}
+
+void open_file(int x){
+    int i, info;
+    char name[A]="name_of_file.txt";
+    cout<<"Atveram failu: "<<name<<endl;
+    ifstream (f2);
+    f2.open(name, ios::in);
+        for (i = 1; i <= x; i++)
+        {
+        f2>>info;
+        cout<<info<<endl;
+        }
+    f2.close();
+
+}
+
+
+
+int main(){
+int x;
+cout<<"Ievadiet skaitļus: "<<endl;cin>>x;
+
+save_file(x);
+open_file(x);
+
 
 system("pause>nul");
 return 0;
