3072bcdc413214a6a818d6c6075a893538d67c15
hub-alex <75374916+hub-alex@users.noreply.github.com>
2024-02-18 08:24:57 +0000

    binarais

---
+++ alg_03_binarais.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+#define N 100
+
+
+using namespace std;
+
+void ievads(int mas[], int n){
+int i;    
+cout<<"\nMas'iva ievads: ";
+for (i=0;i<n;i++) {
+cout<<"mas["<<i<<"]=";
+cin>>mas[i];}
+    }
+
+//-------------    
+
+
+
+void izvads(int mas[], int n){
+int i;
+cout<<"\nMas'iva izvads: ";
+for (i=0;i<n;i++) cout<<mas[i]<<" ";
+    }
+
+//-------------    
+
+//
+void binarais(int mas[], int n, int x){
+int i, kreisa=0, laba=n-1;
+
+    for(i=(kreisa+laba)/2;kreisa<=laba && mas[i]!=x;i=(kreisa+laba)/2){
+    if (mas[i]< x) kreisa = i+1;
+             else laba=i-1;
+        }
+cout<<endl<<endl;
+if (mas[i]==x)
+    cout<<"\nAtrada!";
+else
+    cout<<"\nNeatrada!";
+
+}
+
+
+
+//----------------
+int main(){
+int mas[N];
+int n, x, alg;
+char atbilde;
+cout<<"Mas'iva izm'ers: "; cin>>n;
+ievads(mas, n);
+cout<<"\nMeklejamais cipars: "; cin>>x;
+
+
+izvads(mas, n);
+binarais(mas, n, x);
+
+system("pause>nul");
+return 0;
+}
